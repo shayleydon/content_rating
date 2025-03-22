9e6ca754a50e7732b15a3d52ebd6aa00851878da
shayleydon <shayleydon>
2025-03-22 14:18:10 +0000

    docs

--- src/Components/ContentRating.jsx
+++ src/Components/ContentRating.jsx
@@ -1,18 +1,45 @@
+     import React, { Component } from 'react';
+     import './ContentRating.css';
 
-import React, { Component } from 'react';
-import './ContentRating.css';
+     class ContentRating extends Component {
+       constructor() {
+         super();
+         this.state = {
+             likes: 0,
+             dislikes: 0,
+              handleLike: () => {
+                this.setState((prevState) => ({
+                  likes: prevState.likes + 1,
+                  totalRatings: prevState.totalRatings + 1
+                }));
+              },
+              handleDislike: () => {
+                this.setState((prevState) => ({
+                  dislikes: prevState.dislikes + 1,
+                  totalRatings: prevState.totalRatings + 1
+                }));
+              }
+           };
+       }
+       render() {
+         return (
+          <>
+          <h1>Text Content Rating</h1>
+          <div className='content-rating'>
+             <p>Total Ratings: {this.state.totalRatings}</p>
+             <div className='rating-buttons'>
+             <button className="like-button" onClick={this.state.handleLike}>
+                 Like ({this.state.likes})
+               </button>
+               <button className="dislike-button" onClick={this.state.handleDislike}>
+                 Dislike ({this.state.dislikes})
+               </button>
+             </div>
 
-class ContentRating extends Component {
-  constructor() {
-    super();
-  }
-  render() {
-    return (
-     <>
-     <h1>Text Content Rating</h1>
-     </>
-    );
-  }
-}
+          </div>
+          </>
+         );
+       }
+     }
 
-export default ContentRating;
+     export default ContentRating;
